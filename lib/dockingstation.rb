6d40559eead52bcaf5b7a78b1308108723bd2ea3
Sophie <sophie.k.mclean@btinternet.com>
2017-02-16 15:45:11 +0000

    part 14

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,15 +1,26 @@
  require_relative "bike"
 class DockingStation
+
+  def initialize
+    @bikes = []
+  end
+
+    attr_reader :bikes
+
   def release_bike
-     raise 'No bikes currently available' unless @bike
-    @bike
+     raise 'No bikes currently available' if @bikes.empty?
+    chosen_bike = @bikes.sample
+    @bikes.delete(chosen_bike)
+    chosen_bike
+
   end
 
   def dock(bike)
-     raise "Docking station full" unless !@bike
-     @bike = bike
+     raise "Docking station full" if @bikes.count >= 20
+     @bikes << bike
+      bike
   end
 
-  attr_reader :bike
+
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,29 +1,28 @@
 require "dockingstation"
 
 describe DockingStation do
-it {is_expected.to respond_to(:release_bike)}
+
  describe "#dock_bike"do
 
+ it {is_expected.to respond_to(:dock).with(1).argument}
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
 
-  it "raises and error when the station isn't empty" do
-    bike = Bike.new
-    subject.dock(bike)
-  expect { subject.dock(bike)}.to raise_error "Docking station full"
+
+  it "raises and error when the station is full" do
+    bike =Bike.new
+    20.times{subject.dock(bike)}
+    expect {subject.dock(bike)}.to raise_error "Docking station full"
   end
  end
 
 
+
   describe '#release_bike' do
+    it {is_expected.to respond_to(:release_bike)}
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
@@ -41,5 +40,5 @@ it {is_expected.to respond_to(:release_bike)}
     end
 
   end
-
+it {is_expected.to respond_to(:bikes)}
 end
